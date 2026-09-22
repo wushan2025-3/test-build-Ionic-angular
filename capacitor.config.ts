b14d041d3d01e0ec30b0747147751c1830f71a22
luoyu <luoyu@U-6MXK0F1Q-0019.local>
2026-09-22 17:02:52 +0800

    Add Ionic markers: capacitor.config.ts, ionic scripts, updated ionic.config.json

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.test',
+  appName: 'test-build-Ionic-angular',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
